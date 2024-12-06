a5c9bf611952cfb9cadab77fb8da6853556e64cb
Yuming Huang <987297028@qq.com>
2024-12-06 20:21:58 +0800

    Example of constant propagation in ./src/start.cpp

--- src/start.cpp
+++ src/start.cpp
@@ -20,8 +20,31 @@ Test create_test_2() {
 int main() {
 	std::cout << "Just a test" << '\n';
 
-	Test __attribute__((unused)) test1 = create_test();
-	Test __attribute__((unused))  test2 = create_test_2();
+	// Test __attribute__((unused)) test1 = create_test();
+	// Test __attribute__((unused))  test2 = create_test_2();
+	
+	const int a = 10;
+	std::cout << "const int a is " << a << '\n';
+
+	int *pa = (int *)&a;
+	std::cout << "*pa is " << *pa << '\n';
+
+	*pa = 20;
+	std::cout << "*pa is " << *pa << '\n';
+
+	std::cout << "a is " << a << '\n';
+	std::cout << "a is " << *(&a) << '\n';
+
+	const int c = a * 5;
+	std::cout << "c is " << c << '\n';
+
+	int *pc = (int *)&c;
+	*pc = 500;
+
+	std::cout << "*pc is " << *pc << '\n';
+
+	std::cout << "c is " << c << '\n';
+	std::cout << "c is " << *(&c) << '\n';
 
 	return 0;
 }
